756ed823023bbb59b3d151699a502524da5d8f68
Rodrigo <rodrigo.cavicchioli@outlook.com>
2026-01-11 01:22:06 -0300

    Ajuste nas credenciais de acesso ao MySQL

--- src/conectaDAO.java
+++ src/conectaDAO.java
@@ -22,7 +22,7 @@ public class conectaDAO {
         
         try {
         
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=Digo2211!");
             
         } catch (SQLException erro){
             JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
